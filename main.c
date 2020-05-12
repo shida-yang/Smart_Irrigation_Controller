42d9f9f4cccd2c7fa2d54810e8c2534b14da6835
shida-yang <32109151+shida-yang@users.noreply.github.com>
2020-05-11 23:21:35 -0400

    removed button testing code

--- main.c
+++ main.c
@@ -3,74 +3,6 @@
 #include "weatherServer.h"
 #include "GUI.h"
 
-/**
- * main.c
- */
-
-volatile uint8_t brightness=30;
-
-button_t bt3={
-              135,
-              70,
-              50,
-              20,
-              LCD_YELLOW,
-              "0"
-};
-
-button_t bt1={
-             240,
-             150,
-             50,
-             20,
-             LCD_GREEN,
-             "+"
-};
-
-button_t bt2={
-             80,
-             150,
-             50,
-             20,
-             LCD_RED,
-             "-"
-};
-
-uint8_t count=0;
-
-void PORT4_IRQHandler(){
-    if(P4->IV==2){
-        P4->IE &= (~BIT0);
-        P4->IFG &= (~BIT0);
-        BGT_WaitForTap();
-    }
-}
-
-void BGT_WaitForTap(){
-    DelayMs(50);
-    if((P4->IN & BIT0)==0){
-        //read coordinate
-        Point p;
-        p=TP_ReadXY();
-
-        // inc
-        if(buttonTouched(&bt1, p.x, p.y)){
-            count++;
-        }
-
-        // dec
-        if(buttonTouched(&bt2, p.x, p.y)){
-            count--;
-        }
-
-        sprintf(bt3.text, "%d", count);
-
-        drawButton(&bt3);
-    }
-    P4->IFG &= (~BIT0);
-    P4->IE |= BIT0;
-}
-
 
 void main(void)
 {
@@ -85,19 +17,7 @@ void main(void)
 //    fillInWeekWeatherData();
 
     LCD_Clear(LCD_WHITE);
-//    LCD_Text_size(0, 0, "15:33:26", LCD_BLUE, 3);
-
-
-
-    drawButton(&bt1);
-
-
-
-    drawButton(&bt2);
-
-    drawButton(&bt3);
-
-    NVIC_EnableIRQ(PORT4_IRQn);
+    LCD_Text_size(0, 0, "15:33:26", LCD_BLUE, 3);
 
     while(1);
 }
