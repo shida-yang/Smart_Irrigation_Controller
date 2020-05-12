185615f09f9175b1b02c76f93595064b222797ec
shida-yang <32109151+shida-yang@users.noreply.github.com>
2020-05-11 23:42:04 -0400

    gitignore fixed

--- Debug/BoardSupportPackage/src/bme280.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-BoardSupportPackage/src/bme280.obj: ../BoardSupportPackage/src/bme280.c
-BoardSupportPackage/src/bme280.obj: E:/CCS_Workspace/Smart_Irrigation_Controller/BoardSupportPackage/inc/bme280.h
-
-../BoardSupportPackage/src/bme280.c: 
-E:/CCS_Workspace/Smart_Irrigation_Controller/BoardSupportPackage/inc/bme280.h: 
--- Debug/BoardSupportPackage/src/bmi160.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-BoardSupportPackage/src/bmi160.obj: ../BoardSupportPackage/src/bmi160.c
-BoardSupportPackage/src/bmi160.obj: E:/CCS_Workspace/Smart_Irrigation_Controller/BoardSupportPackage/inc/bmi160.h
-
-../BoardSupportPackage/src/bmi160.c: 
-E:/CCS_Workspace/Smart_Irrigation_Controller/BoardSupportPackage/inc/bmi160.h: 
